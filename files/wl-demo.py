c6bd01a519bdbd8b9a69895d01b8d44cda72ad03
wilhelm43 <105068971+WWX-Team@users.noreply.github.com>
2024-09-16 21:27:50 +0200

    Update wl-demo.py
    [upd] 00110

--- files/wl-demo.py
+++ files/wl-demo.py
@@ -1,35 +1,41 @@
 import wL as wLpy
-from time import time
 
-as_range = 1000
+as_range = 1
 
-print('wL Demo /')
+print('\nwL Demo / STR ––––––––')
 
-myWL = wLpy.wL()
-myWL.dict = {'example' : ['pêche_au_thon', 'chocolatine'], 'city' : {'Toulouse' : {'population' : 510000, 'loc' : 'south-west'}, 'Paris' : {'population' : 2380000, 'loc' : 'north'}}}
-packed = myWL.pack()
-print(packed)
+def wlTests():
+    myWL = wLpy.wL()
+    myWL.dict = {'example' : ['pêche au thon', 'chocolatine'], 'city' : {'Toulouse' : {'population' : 510000, 'loc' : 'south-west'}, 'Paris' : {'population' : 2380000, 'loc' : 'north'}}}
+    packed = myWL.pack()
+    print(packed)
+    moWL = wLpy.wL()
+    moWL.unpack(file = packed)
+    print(moWL.get())
+    print(moWL.exportXML())
 
-moWL = wLpy.wL()
-moWL.unpack(file = myWL.pack())
-print(moWL.get())
-print(moWL.exportXML())
+def wLPerfs():
+    myWL = wLpy.wL()
+    myWL.dict = {'example' : ['pêche au thon', 'chocolatine'], 'city' : {'Toulouse' : {'population' : 510000, 'loc' : 'south-west'}, 'Paris' : {'population' : 2380000, 'loc' : 'north'}}}
+    packed = myWL.pack()
+    from time import time
+    print('wL Demo / PERFS')
+    print(f' - range({as_range})')
+    print('wL Demo / pack.perfs /')
+    as_time  = time()
+    for _ in range(as_range):
+        myWL.pack()
+    total_time = round(1000 * (time() - as_time)) / 1000
+    print(f' - Done in {total_time} seconds. \n')
+    print('wL Demo / unpack.perfs /')
+    as_time  = time()
+    for _ in range(as_range):
+        myWL.unpack(packed)
+    total_time = round(1000 * (time() - as_time)) / 1000
+    print(f' - Done in {total_time} seconds, for {len(packed) * as_range} chr. \n')
 
-# wL_info()
+wlTests()
+#wLpy.wL_info()
+#wLPerfs()
 
-print('wL Demo / PERFS')
-print(f' - range({as_range})')
-print('wL Demo / pack.perfs /')
-as_time  = time()
-for _ in range(as_range):
-    myWL.pack()
-total_time = round(1000 * (time() - as_time)) / 1000
-print(f' - Done in {total_time} seconds. \n')
-print('wL Demo / unpack.perfs /')
-as_time  = time()
-for _ in range(as_range):
-    myWL.unpack(packed)
-total_time = round(1000 * (time() - as_time)) / 1000
-print(f' - Done in {total_time} seconds, for {len(packed) * as_range} chr. \n')
-
-print('wL Demo / \n')
+print('wL Demo / END ––––––––\n')
