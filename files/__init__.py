0c476af399a8c0c55d5bfcfcd66475f7bc2a090b
wilhelm43 <wilhelmbismarck@tutanota.de>
2025-07-16 19:32:00 +0200

    Update __init__.py

--- files/__init__.py
+++ files/__init__.py
@@ -5,7 +5,7 @@ wL is a "markup" "metalanguage" designed to store arbitrary data in a format tha
 The design goals of wL emphasize elementary simplicity, generality, stability, and usability across all programming language.
 
 - wL by       : wilhelm43 ;
-- lib version : 1.3.2 ;
+- lib version : 1.3.3 ;
 - supports wL : 1.0, 1.1 ;
 
 
@@ -27,5 +27,5 @@ Consider checking each sub packages documentation.
 
 supports : tuple[str] = ("1.1", "1.0")
 """Supported wL versions by `parser`. The first is always the one used by `exporter`."""
-version  : str        = "1.3.2"
+version  : str        = "1.3.3"
 """wL package version"""
